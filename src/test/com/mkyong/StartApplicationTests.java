05231e7a71b5520f52d10baff58c10c9c4ab5228
Shashimal <Shashimal>
2021-08-06 02:36:53 +0800

    fix test issue

---
+++ src/test/com/mkyong/StartApplicationTests.java
@@ -0,0 +1,15 @@
+package com.mkyong;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartApplicationTests {
+
+    @Test
+    void contextLoads() {
+        Assert.assertEquals("hello", "hello");
+    }
+
+}
